8306bba8c0f113d56fed8ce6df93584770e339f6
Pratik Tamgole <pat79775@outlook.com>
2024-12-08 22:07:15 +0530

    Adding the solution for Day-08 in Go

--- C/Day-8/problem-1/problem-1-solution.c
+++ C/Day-8/problem-1/problem-1-solution.c
@@ -1,73 +1,64 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <assert.h>
 
-#define INPUT 
 
-#ifdef INPUT
-#define FILE_NAME "inputdata.txt"
-#define SIZE 50
-#endif
-
-typedef struct {
-    int r;
-    int c;
-} Position;
-
-typedef struct {
-    char freq;
-    int count;
-    Position antennas[100];
-} Frequency;
-
-int main() {
-    FILE* f = fopen(FILE_NAME, "r");
-
-    char map[SIZE][SIZE];
-
-    Frequency frequencyTable[256] = {0};
-
-    for (int r = 0; r < SIZE; r++) {
-        for (int c = 0; c < SIZE; c++) {
-            fscanf(f, "%c ", &map[r][c]);
-            if (map[r][c] != '.') {
-                frequencyTable[map[r][c]].freq = map[r][c];
-                frequencyTable[map[r][c]].antennas[frequencyTable[map[r][c]].count] = (Position){r,c};
-                frequencyTable[map[r][c]].count ++;
-            }
-        }
+int main (int argc, char * argv[])
+{
+    char *filename = "inputdata.txt";
+    long result=0;
+    int part=1;
+    char buf[55];
+    char grid[55][55]={0};
+    char nodes[55][55]={0};
+    int size=0;
+    FILE * fp;
+    if(argc > 1)
+        part=atoi(argv[1]);
+    if(argc > 2)
+        filename=argv[2];
+    if((fp = fopen (filename, "r"))==NULL)
+    {
+        printf("error opening file %s\n", filename);
+        return 1;
     }
-    for (int i = 0; i < 256; i++) {
-        Frequency freq = frequencyTable[i];
-        if (freq.freq != 0) {
-            for (int j = 0; j < freq.count-1; j++) {
-                Position a = freq.antennas[j];
-                for (int k = j+1; k < freq.count; k++) {
-                    Position b = freq.antennas[k];
-                    int diffRow = b.r - a.r;
-                    int diffCol = b.c - a.c;
-                    Position c = {a.r - diffRow, a.c - diffCol};
-                    Position d = {b.r + diffRow, b.c + diffCol};
-                    if (c.r >= 0 && c.r < SIZE && c.c >= 0 && c.c < SIZE) {
-                        map[c.r][c.c] = '#';
-                    }
-                    if (d.r >= 0 && d.r < SIZE && d.c >= 0 && d.c < SIZE) {
-                        map[d.r][d.c] = '#';
+    while(fgets(buf, 55, fp) != NULL)
+    {
+        strncpy(grid[size++],buf,sizeof(grid[size]));
+    }
+    for(int x=0;x<size;x++)
+        for(int y=0;y<size;y++)
+        {
+            if(grid[x][y]!='.')
+            for(int x2=x;x2<size;x2++)
+                for(int y2=0;y2<size;y2++)
+                {
+                    if(x2==x && y2<= y)
+                        continue;
+                    if(grid[x][y]== grid[x2][y2])
+                    {
+                        int slopex=x2-x;
+                        int slopey=y2-y;
+                        if(part==1)
+                        {
+                            if((x-slopex<size)&&(x-slopex>=0)&&(y-slopey<size)&&(y-slopey>=0))
+                                nodes[x-slopex][y-slopey]=1;
+                            if((x2+slopex<size)&&(x2+slopex>=0)&&(y2+slopey<size)&&(y2+slopey>=0))
+                                nodes[x2+slopex][y2+slopey]=1;
+                        }
+                        else
+                        {
+                            for(int i=0;(x-(slopex*i)<size)&&(x-(i*slopex)>=0)&&(y-(i*slopey)<size)&&(y-(i*slopey)>=0);i++)
+                                nodes[x-(i*slopex)][y-(i*slopey)]=1;
+                            for(int i=1;(x+(slopex*i)<size)&&(x+(i*slopex)>=0)&&(y+(i*slopey)<size)&&(y+(i*slopey)>=0);i++)
+                                nodes[x+(i*slopex)][y+(i*slopey)]=1;
+                        }
                     }
                 }
-            }
         }
-    }
-    int count = 0;
-    for (int r = 0; r < SIZE; r++) {
-        for (int c = 0; c < SIZE; c++) {
-            printf("%c", map[r][c]);
-            if (map[r][c] == '#') {
-                count ++;
-            }
-        }
-        printf("\n");
-    }
-    printf("%d\n", count);
+    for(int x=0;x<size;x++)
+        for(int y=0;y<size;y++)
+            result+=nodes[x][y];
+    printf("%ld\n",result);
+    return 0;
 }
---
+++ Go/Day-8/problem-1/problem-1.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+const Size = 50
+
+// Point structure to represent grid coordinates
+type P struct {
+	X, Y int
+}
+
+// Check if a point is within bounds
+func (p P) Valid() bool {
+	return p.X >= 0 && p.X < Size && p.Y >= 0 && p.Y < Size
+}
+
+// Parse input from "inputdata.txt" to build a frequency map
+func parseInput() map[byte][]P {
+	file, err := os.Open("inputdata.txt")
+	if err != nil {
+		fmt.Println("Error: Unable to open input file.")
+		os.Exit(1)
+	}
+	defer file.Close()
+
+	freqs := map[byte][]P{}
+	scanner := bufio.NewScanner(file)
+
+	for i := 0; scanner.Scan(); i++ {
+		line := scanner.Text()
+
+		for j, c := range line {
+			if c == '.' {
+				continue
+			}
+
+			ch := byte(c)
+
+			freqs[ch] = append(freqs[ch], P{i, j})
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Println("Error: Unable to read input file.")
+		os.Exit(1)
+	}
+
+	return freqs
+}
+
+func main() {
+	// Parse the input file
+	freqs := parseInput()
+
+	// Map to store unique antinodes
+	antiNodes := map[P]struct{}{}
+
+	// Process each frequency group
+	for _, locs := range freqs {
+		// Iterate over all pairs of antennas
+		for a := 0; a < len(locs)-1; a++ {
+			for b := a + 1; b < len(locs); b++ {
+				// Calculate the delta between two antennas
+				delta := P{locs[b].X - locs[a].X, locs[b].Y - locs[a].Y}
+
+				// Calculate antinode 1 by subtracting delta
+				anti1 := P{locs[a].X - delta.X, locs[a].Y - delta.Y}
+				if anti1.Valid() {
+					antiNodes[anti1] = struct{}{}
+				}
+
+				// Calculate antinode 2 by adding delta
+				anti2 := P{locs[b].X + delta.X, locs[b].Y + delta.Y}
+				if anti2.Valid() {
+					antiNodes[anti2] = struct{}{}
+				}
+			}
+		}
+	}
+
+	// Print the total count of unique antinode locations
+	fmt.Println(len(antiNodes))
+}
