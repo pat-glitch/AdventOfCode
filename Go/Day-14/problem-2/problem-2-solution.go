637d015df22d363cef173d99c8030271866b156d
Pratik Tamgole <pat79775@outlook.com>
2024-12-15 11:01:55 +0530

    Adding the correct solutions for Day-1 and Day-2

--- Go/Day-14/problem-2/problem-2-solution.go
+++ Go/Day-14/problem-2/problem-2-solution.go
@@ -1,123 +1,124 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-	"io/ioutil"
+	"log"
+	"os"
+	"strconv"
 	"strings"
 )
 
-type posT struct {
-	x, y int
+const (
+	RoomWidth   = 101
+	RoomHeight  = 103
+	TimeElapsed = 100
+)
+
+type Robot struct {
+	x, y   int // Position as grid coordinates
+	vx, vy int // Velocity in tiles per second
 }
 
-// Function to handle modulo with positive results
-func mod(a, b posT) posT {
-	return posT{(a.x%b.x + b.x) % b.x, (a.y%b.y + b.y) % b.y}
+func (r *Robot) UpdatePosition(time int) {
+	r.x = discreteWrap(r.x+r.vx*time, RoomWidth)
+	r.y = discreteWrap(r.y+r.vy*time, RoomHeight)
 }
 
-// The Robot struct holds position and velocity for each robot
-type robotT struct {
-	p, v posT
-}
-
-// Load the input from the given file
-func loadInput(filename string) []robotT {
-	data, err := ioutil.ReadFile(filename)
-	if err != nil {
-		fmt.Println("Error reading file:", err)
-		return nil
+func discreteWrap(pos, size int) int {
+	for pos < 0 {
+		pos += size
 	}
-
-	lines := strings.Split(string(data), "\n")
-	var robots []robotT
-	for _, line := range lines {
-		var r robotT
-		_, err := fmt.Sscanf(line, "p=%d,%d v=%d,%d", &r.p.x, &r.p.y, &r.v.x, &r.v.y)
-		if err != nil {
-			continue
-		}
-		robots = append(robots, r)
-	}
-	return robots
+	return pos % size
 }
 
-// Part 1 solution function
-func part1(robots []robotT, size posT) int {
-	quads := [4]int{0, 0, 0, 0}
-
-	for i := range robots {
-		robots[i].p = mod(posT{robots[i].p.x + robots[i].v.x*100, robots[i].p.y + robots[i].v.y*100}, size)
-		if robots[i].p.x != size.x/2 && robots[i].p.y != size.y/2 {
-			ix := 0
-			if robots[i].p.y < size.y/2 {
-				ix = 1
-			}
-			iy := 0
-			if robots[i].p.x < size.x/2 {
-				iy = 1
-			}
-			quads[iy*2+ix]++
-		}
+func determineQuadrant(x, y int) int {
+	if x == RoomWidth/2 || y == RoomHeight/2 {
+		return -1 // Not in any quadrant
 	}
-
-	return quads[0] * quads[1] * quads[2] * quads[3]
-}
-
-// Depth-first search to find clusters of robots
-func dfs(curr posT, points map[posT]bool, visited map[posT]bool) int {
-	if visited[curr] {
+	if x < RoomWidth/2 && y < RoomHeight/2 {
 		return 0
+	} else if x > RoomWidth/2 && y < RoomHeight/2 {
+		return 1
+	} else if x < RoomWidth/2 && y > RoomHeight/2 {
+		return 2
+	} else {
+		return 3
 	}
-	visited[curr] = true
-	clusterSize := 1
-
-	directions := []posT{{0, 1}, {1, 0}, {0, -1}, {-1, 0}}
-	for _, d := range directions {
-		newPos := posT{curr.x + d.x, curr.y + d.y}
-		if points[newPos] && !visited[newPos] {
-			clusterSize += dfs(newPos, points, visited)
-		}
-	}
-	return clusterSize
 }
 
-// Find a cluster of robots greater than a threshold
-func findCluster(bots []robotT, threshold int) bool {
-	points := make(map[posT]bool)
-	for _, bot := range bots {
-		points[bot.p] = true
+func forward(robots []Robot, time int) []Robot {
+	updatedRobots := make([]Robot, len(robots))
+	for i, robot := range robots {
+		robot.UpdatePosition(time)
+		updatedRobots[i] = robot
 	}
-
-	visited := make(map[posT]bool)
-	for bot := range points {
-		clusterSize := dfs(bot, points, visited)
-		if clusterSize >= threshold {
-			return true
-		}
-	}
-	return false
+	return updatedRobots
 }
 
-// Part 2 solution function
-func part2(bots []robotT, size posT) int {
-	seconds := 0
-
-	for !findCluster(bots, 40) {
-		for i := range bots {
-			bots[i].p = mod(posT{bots[i].p.x + bots[i].v.x, bots[i].p.y + bots[i].v.y}, size)
+func calculateQuadrants(robots []Robot) []int {
+	u := RoomWidth / 2
+	v := RoomHeight / 2
+	quadrants := make([]int, 4)
+	for _, robot := range robots {
+		x, y := robot.x, robot.y
+		if x < u && y < v {
+			quadrants[0]++
+		} else if x > u && y < v {
+			quadrants[1]++
+		} else if x < u && y > v {
+			quadrants[2]++
+		} else if x > u && y > v {
+			quadrants[3]++
 		}
-		seconds++
 	}
+	return quadrants
+}
 
-	return seconds
+func part1(robots []Robot) int {
+	quadrants := calculateQuadrants(forward(robots, TimeElapsed))
+	safetyFactor := 1
+	for _, count := range quadrants {
+		safetyFactor *= count
+	}
+	return safetyFactor
+}
+
+func part2(robots []Robot) int {
+	for t := 0; ; t++ {
+		updatedPositions := forward(robots, t)
+		positionSet := make(map[string]struct{})
+		for _, robot := range updatedPositions {
+			key := fmt.Sprintf("%d,%d", robot.x, robot.y)
+			positionSet[key] = struct{}{}
+		}
+		if len(positionSet) == len(robots) {
+			return t
+		}
+	}
 }
 
 func main() {
-	// Load input
-	actualValues := loadInput("inputdata.txt")
+	file, err := os.Open("inputdata.txt")
+	if err != nil {
+		log.Fatal("Error opening input file:", err)
+	}
+	defer file.Close()
 
-	fmt.Println("part1:", part1(actualValues, posT{101, 103}))
+	var robots []Robot
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		parts := strings.Split(line, " ")
+		pParts := strings.Split(strings.TrimPrefix(parts[0], "p="), ",")
+		x, _ := strconv.Atoi(pParts[0])
+		y, _ := strconv.Atoi(pParts[1])
+		vParts := strings.Split(strings.TrimPrefix(parts[1], "v="), ",")
+		vx, _ := strconv.Atoi(vParts[0])
+		vy, _ := strconv.Atoi(vParts[1])
+		robots = append(robots, Robot{x: x, y: y, vx: vx, vy: vy})
+	}
 
-	// Part 2
-	fmt.Println("part2:", part2(actualValues, posT{101, 103}))
+	fmt.Println("Part 1 Safety Factor:", part1(robots))
+	fmt.Println("Part 2 Time Until All Robots Diverge:", part2(robots))
 }
